1264344663d41201b6c684d455deeb0b76afaf1f
Antonio Leiva <antonio@devexperto.com>
2021-12-10 11:53:41 +0100

    [MC] Use LaunchedEffect to handle back action

--- MarvelCompose/app/src/main/java/com/antonioleiva/marvelcompose/ui/screens/common/MarvelItemsListScreen.kt
+++ MarvelCompose/app/src/main/java/com/antonioleiva/marvelcompose/ui/screens/common/MarvelItemsListScreen.kt
@@ -1,5 +1,7 @@
 package com.antonioleiva.marvelcompose.ui.screens.common
 
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.LocalOnBackPressedDispatcherOwner
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -12,9 +14,11 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import com.antonioleiva.marvelcompose.data.entities.MarvelItem
 import com.antonioleiva.marvelcompose.data.entities.Result
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @ExperimentalFoundationApi
@@ -28,12 +32,22 @@ fun <T : MarvelItem> MarvelItemsListScreen(
     items.fold({ ErrorMessage(it) }) { marvelItems ->
         var bottomSheetItem by remember { mutableStateOf<T?>(null) }
         val sheetState = rememberModalBottomSheetState(ModalBottomSheetValue.Hidden)
+        val scope = rememberCoroutineScope()
+
+        BackPressedHandler {
+            scope.launch { sheetState.hide() }
+        }
 
         ModalBottomSheetLayout(
             sheetContent = {
                 MarvelItemBottomPreview(
                     item = bottomSheetItem,
-                    onGoToDetail = onClick
+                    onGoToDetail = {
+                        scope.launch {
+                            sheetState.hide()
+                            onClick(it)
+                        }
+                    }
                 )
             },
             sheetState = sheetState
@@ -42,9 +56,29 @@ fun <T : MarvelItem> MarvelItemsListScreen(
                 loading = loading,
                 items = marvelItems,
                 onItemClick = onClick,
-                onItemMore = { bottomSheetItem = it }
+                onItemMore = {
+                    scope.launch {
+                        bottomSheetItem = it
+                        sheetState.show()
+                    }
+                }
             )
         }
+    }
+}
+
+@Composable
+fun BackPressedHandler(onBack: () -> Unit) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val backDispatcher =
+        requireNotNull(LocalOnBackPressedDispatcherOwner.current).onBackPressedDispatcher
+
+    LaunchedEffect(lifecycleOwner, backDispatcher) {
+        backDispatcher.addCallback(lifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                onBack()
+            }
+        })
     }
 }
 
