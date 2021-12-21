d6b747e295ad82f18d5f69b24bb341b9eedf0f1d
Antonio Leiva <antonio@devexperto.com>
2021-12-21 12:30:33 +0100

    [MC] Update theme colors

--- MarvelCompose/app/src/main/java/com/antonioleiva/marvelcompose/ui/theme/Color.kt
+++ MarvelCompose/app/src/main/java/com/antonioleiva/marvelcompose/ui/theme/Color.kt
@@ -2,7 +2,6 @@ package com.antonioleiva.marvelcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Red = Color(0xFFE23636)
+val RedDark = Color(0xFF791111)
+val Blue = Color(0xFF518CCA)
