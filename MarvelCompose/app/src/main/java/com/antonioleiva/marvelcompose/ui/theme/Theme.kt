70784161e5f0a115260ac6bdcdf76f81380f54b6
antoniolg <antonio@devexperto.com>
2022-12-26 12:30:22 +0100

    Apply M3 Colors

--- MarvelCompose/app/src/main/java/com/antonioleiva/marvelcompose/ui/theme/Theme.kt
+++ MarvelCompose/app/src/main/java/com/antonioleiva/marvelcompose/ui/theme/Theme.kt
@@ -1,32 +1,23 @@
 package com.antonioleiva.marvelcompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette = darkColorScheme(
     primary = Red,
-    primaryVariant = RedDark,
-    secondary = Blue,
+    secondary = RedDark,
+    tertiary = Blue,
     onPrimary = Color.White,
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorPalette = lightColorScheme(
     primary = Red,
-    primaryVariant = RedDark,
-    secondary = Blue
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    secondary = RedDark,
+    tertiary = Blue
 )
 
 @Composable
@@ -41,9 +32,9 @@ fun MarvelComposeTheme(
     }
 
     MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
+        colorScheme = colors,
+        //typography = Typography,
+        //shapes = Shapes,
         content = content
     )
 }
